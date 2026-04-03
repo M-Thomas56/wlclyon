12c527ba5f6c072312bd2def4a69a6e37f5bbef7
marc thomas <marc.thomas5608@gmail.com>
2026-04-03 12:11:03 +0200

    model ok

---
+++ db/migrate/20260403100705_create_places.rb
@@ -0,0 +1,14 @@
+class CreatePlaces < ActiveRecord::Migration[8.1]
+  def change
+    create_table :places do |t|
+      t.string :title
+      t.text :content
+      t.float :latitude
+      t.float :longitude
+      t.references :user, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
